e5ce32a0645812121ff248210a799240b2d8a4d3
조영재 <wo_123@naver.com>
2023-01-31 00:57:49 +0900

    footer 작업

--- src/Footer/Footer.styled.ts
+++ src/Footer/Footer.styled.ts
@@ -9,15 +9,24 @@ export const DivWrap = styled.div`
     display:flex;
     justify-content: space-evenly;
     align-items: center;
-`
+    padding: 10px;
+    `
 
 export const ItemDiv = styled.div`
 `
 
+export const Atag = styled.a`
+
+`
+
 export const FooterImg = styled.img`
-    width: 100px;
-    height: 100px;
+    width: 50px;
+    height: 50px;
+    border-radius: 50px;
     :hover{
+        border-radius: 50px;
         cursor: pointer;
+        width: 60px;
+        height: 60px;
     }
 `
--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import * as S from './Footer.styled';
-import git from "../../resource/logo/free-icon-github-logo-25231.png";
+import git from "../../resource/logo/github-gf55eb9d5f_640.png";
+import youtube from "../../resource/logo/로고다운로드-유튜브로고 아이콘-9.png";
+import fifa from "../../resource/logo/mobile_180.png";
+
+
 export const Footer = () => {
     const gitUrl = "https://github.com/dudwo1996";
     return (
         <S.Container>
             <S.DivWrap>
-                <S.ItemDiv><S.FooterImg src={git} /></S.ItemDiv>
-                <S.ItemDiv><S.FooterImg src={git} /></S.ItemDiv>
-                <S.ItemDiv><S.FooterImg src={git} /></S.ItemDiv>
-                <S.ItemDiv><S.FooterImg src={git} /></S.ItemDiv>
+                <S.ItemDiv><S.Atag href="http://www.github.com/dudwo1996"><S.FooterImg src={git} /></S.Atag></S.ItemDiv>
+                <S.ItemDiv><S.Atag href="https://www.youtube.com/@EASPORTSTMFIFAONLINE"><S.FooterImg src={youtube} /></S.Atag></S.ItemDiv>
+                <S.ItemDiv><S.Atag href="https://fifaonline4.nexon.com/main/index"><S.FooterImg src={fifa} /></S.Atag></S.ItemDiv>
             </S.DivWrap>
         </S.Container>
     )
